e80e0d11d5a92b738f5cf314265d27f3659aef5e
xbabio <xosebabiosabin@gmail.com>
2025-10-16 22:26:25 +0200

    build: parent gradle config

---
+++ services/catalog-service/build.gradle.kts
@@ -0,0 +1,29 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version "3.3.5" apply false
+    id("io.spring.dependency-management") version "1.1.6" apply false
+}
+
+allprojects {
+    group = "com.flowcart"
+    version = "0.0.1-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects {
+    apply(plugin = "java")
+    apply(plugin = "io.spring.dependency-management")
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_21
+    }
+    
+    dependencies {
+        compileOnly("org.projectlombok:lombok")
+        annotationProcessor("org.projectlombok:lombok")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
+}
---
+++ services/catalog-service/settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "catalog-service"
+
+include(
+    "catalog-app",
+    "catalog",
+    "catalog-api-contract",
+    "catalog-domain-events"
+)
